4e8c6ab86ef770357bc784b4d1edf95db2d334d8
Anthony Khon <anthony.khon@gmail.com>
2021-06-18 10:57:32 -0400

    added .js files

--- app.js
+++ app.js
@@ -4,7 +4,17 @@ console.log(
 );
 
 // Exercise 1
+const firstName = "Anthony ";
+let lastName = "Khon";
+var age = 30;
 
 // Exercise 2
+let fullName = (firstName + lastName);
+console.log(fullName);
 
 // Exercise 3
+let city = "Milford"
+let passTime = "working on cars"
+let myStory = `\Hello my name is ${fullName}. I live in ${city}. I enjoy
+${passTime} and coding!`;
+console.log(myStory);  
